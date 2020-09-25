2fe6aaed3c0e77a98be40764a87dba0e3a3c2648
hsi-cy <hsichengyun@gmail.com>
2020-09-25 13:14:13 +0800

    test out

--- dash_prac.py
+++ dash_prac.py
@@ -1,6 +1,12 @@
 import dash
-import pandas as pd
+import dash_core_components as dcc
+import dash_html_components as html
 
-df = pd.read_csv("intro_bees.csv")
-print("hollo world")
-print(df.head())
+app = dash.Dash()
+app.layout = html.Div([
+    html.H1('Hello World'),
+    html.Div('Dash - A data product development framework from plotly.')
+])
+
+if __name__ == '__main__':
+    app.run_server()
