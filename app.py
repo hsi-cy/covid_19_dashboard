299ed50d238d05f1911eaf37f54246aa4aa7c787
hsi-cy <hsichengyun@gmail.com>
2020-09-30 13:32:45 +0800

    sss

--- app.py
+++ app.py
@@ -39,22 +39,12 @@ app = dash.Dash()
 app.layout = html.Div(
     [
         html.Div([html.H1("COVID-19 Dashboard")], className="title"),
-        html.Div(
-            [
-                html.Div(
-                    [
-                        html.Div(
-                            [
-                                dcc.Dropdown(
-                                    id="countryDropdown",
-                                    options=optCountries,
-                                )
-                            ],
-                            className="dropdown",
-                        ),
-                        html.Div(
-                            [
-                                dcc.RadioItems(
+        html.Div([
+            html.Div([
+                dcc.Dropdown(id='countryDropdown', options=optCountries),
+            ]),
+            html.Div([
+                dcc.RadioItems([
                                     options=[
                                         {"label": "Total Cases", "value": "TTC"},
                                         {"label": "Daily New Cases", "value": "DNC"},
@@ -63,27 +53,13 @@ app.layout = html.Div(
                                     value="TTC",
                                     labelStyle={"display": "inline-block"},
                                     id="graphSelection",
-                                )
-                            ]
-                        ),
-                        html.Div(
-                            [
-                                dcc.Graph(
-                                    id="chosenGraph",
-                                )
-                            ],
-                            className="graph",
-                        ),
-                    ],
-                    className="graphContainer",
-                ),
-                html.Div(
-                    [html.H2("Global data will be here")],
-                    className="globalDataContainer",
-                ),
-            ],
+            ]),
+        html.Div([
+            dcc.Graph(id=chosenGraph)], className="graphContainer")
+        ],
             className="lowerPartContainer",
         ),
+        html.Div([html.H2("Global Data")], className="globalDataContainer"),
     ],
     className="biggestContainer",
 )
@@ -115,63 +91,63 @@ app.layout = html.Div(
 # ], className='biggestContainer')
 
 
-@app.callback(
-    Output(component_id="chosenGraph", component_property="figure"),
-    [
-        Input(component_id="countryDropdown", component_property="value"),
-        Input(component_id="graphSelection", component_property="value"),
-    ],
-)
-def update_figure1(countryDropdown, graphSelection):
-    barColor = "rgb(8,48,107)"
-    if graphSelection == "TTL":
+# @app.callback(
+#     Output(component_id="chosenGraph", component_property="figure"),
+#     [
+#         Input(component_id="countryDropdown", component_property="value"),
+#         Input(component_id="graphSelection", component_property="value"),
+#     ],
+# )
+# def update_figure1(countryDropdown, graphSelection):
+#     barColor = "rgb(8,48,107)"
+#     if graphSelection == "TTL":
 
-        fig = make_subplots(rows=1)
+#         fig = make_subplots(rows=1)
 
-        fig.add_bar(
-            x=dfConfirmed["Date"],
-            y=dfConfirmed[countryDropdown],
-            marker_color="rgb(8,48,107)",
-            marker_line_color="rgb(8,48,107)",
-            marker_line_width=1.5,
-            opacity=1,
-            name="Confirmed",
-        )
-        fig.add_bar(
-            x=dfRecovered["Date"],
-            y=dfRecovered[countryDropdown],
-            marker_color="rgb(102,255,178)",
-            marker_line_color="rgb(102,255,178)",
-            marker_line_width=1.5,
-            opacity=1,
-            name="Recovered",
-        )
-        fig.add_bar(
-            x=dfDeaths["Date"],
-            y=dfDeaths[countryDropdown],
-            marker_color="rgb(255,102,102)",
-            marker_line_color="rgb(255,102,102)",
-            marker_line_width=1.5,
-            opacity=1,
-            name="Deaths",
-        )
+#         fig.add_bar(
+#             x=dfConfirmed["Date"],
+#             y=dfConfirmed[countryDropdown],
+#             marker_color="rgb(8,48,107)",
+#             marker_line_color="rgb(8,48,107)",
+#             marker_line_width=1.5,
+#             opacity=1,
+#             name="Confirmed",
+#         )
+#         fig.add_bar(
+#             x=dfRecovered["Date"],
+#             y=dfRecovered[countryDropdown],
+#             marker_color="rgb(102,255,178)",
+#             marker_line_color="rgb(102,255,178)",
+#             marker_line_width=1.5,
+#             opacity=1,
+#             name="Recovered",
+#         )
+#         fig.add_bar(
+#             x=dfDeaths["Date"],
+#             y=dfDeaths[countryDropdown],
+#             marker_color="rgb(255,102,102)",
+#             marker_line_color="rgb(255,102,102)",
+#             marker_line_width=1.5,
+#             opacity=1,
+#             name="Deaths",
+#         )
 
-        fig.update_layout(title_text="Total Confirmed/Recovered/Deaths Cases")
+#         fig.update_layout(title_text="Total Confirmed/Recovered/Deaths Cases")
 
-        return fig
-    elif graphSelection == "DNC":
-        fig2 = px.bar(dailyGlobalNewCases, x="Date", y=countryDropdown)
-        fig2.update_traces(marker_color=barColor, marker_line_color=barColor, opacity=1)
-        fig2.update_layout(title_text="Daily New Cases")
+#         return fig
+#     elif graphSelection == "DNC":
+#         fig2 = px.bar(dailyGlobalNewCases, x="Date", y=countryDropdown)
+#         fig2.update_traces(marker_color=barColor, marker_line_color=barColor, opacity=1)
+#         fig2.update_layout(title_text="Daily New Cases")
 
-        return fig2
+#         return fig2
 
-    elif graphSelection == "ATC":
-        fig3 = px.bar(dfActive, x="Date", y=countryDropdown)
-        fig3.update_traces(marker_color=barColor, marker_line_color=barColor, opacity=1)
-        fig3.update_layout(title_text="Active Cases")
+#     elif graphSelection == "ATC":
+#         fig3 = px.bar(dfActive, x="Date", y=countryDropdown)
+#         fig3.update_traces(marker_color=barColor, marker_line_color=barColor, opacity=1)
+#         fig3.update_layout(title_text="Active Cases")
 
-        return fig3
+#         return fig3
 
 
 if __name__ == "__main__":
