3360dce6739d525ce0ef90872983110dac2a60d0
hsi-cy <hsichengyun@gmail.com>
2020-10-06 11:38:29 +0800

    dfd

--- bootstrap_test.py
+++ bootstrap_test.py
@@ -6,6 +6,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 from plotly.subplots import make_subplots
+import dash_bootstrap_components as dbc
+
 
 # Read files
 dfConfirmed = pd.read_csv("confirmed.csv").drop("Unnamed: 0", axis=1).set_index("Date")
@@ -41,42 +43,266 @@ ATC = str(round(dfActive.iloc[-1, :].sum() / 1000000, 2)) + "M"
 
 # ------- app start from here -------
 
-app = dash.Dash(external_stylesheets=[dbc.themes.CYBORG])
+app = dash.Dash(external_stylesheets=[dbc.themes.FLATLY])
 
-app.layout = 
-
-@app.callback(
-    Output(component_id="chosenGraph", component_property="figure"),
-    [
-        Input(component_id="countryDropdown", component_property="value"),
-        Input(component_id="graphSelection", component_property="value"),
-    ],
+"""NavBar Start"""
+nav_item = dbc.NavItem(
+    dbc.NavLink(
+        "Data Source",
+        href="https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series",
+    )
 )
-def update_figure(countryDropdown, graphSelection):
-    if graphSelection == "TTC":
-        fig = make_subplots(rows=1, cols=1)
-        fig.add_trace(
-            go.Bar(x=dfConfirmed.index, y=dfConfirmed[countryDropdown]), row=1, col=1
+
+# make a reuseable dropdown for the different examples
+dropdown = dbc.DropdownMenu(
+    children=[
+        dbc.DropdownMenuItem(
+            "Project GitHub",
+            href="https://github.com/hsi-cy/covid_19_dashboard",
+        ),
+        dbc.DropdownMenuItem(
+            "LinkedIn", href="https://www.linkedin.com/in/cheng-yun-hsi-b586b5178/"
+        ),
+        dbc.DropdownMenuItem(divider=True),
+        dbc.DropdownMenuItem("Plotly / Dash", href="https://dash.plot.ly/"),
+        dbc.DropdownMenuItem(
+            "Dash Bootstrap",
+            href="https://dash-bootstrap-components.opensource.faculty.ai/",
+        ),
+    ],
+    nav=True,
+    in_navbar=True,
+    label="Important Links",
+)
+
+# Navbar Layout
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.H2(
+                # Use row and col to control vertical alignment of logo / brand
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            dbc.NavbarBrand("COVID-19 Dashboard", className="ml-2")
+                        ),
+                    ],
+                    align="center",
+                    no_gutters=True,
+                )
+            ),
+            dbc.NavbarToggler(id="navbar-toggler2"),
+            dbc.Collapse(
+                dbc.Nav(
+                    [
+                        nav_item,
+                        dropdown,
+                    ],
+                    className="ml-auto",
+                    navbar=True,
+                ),
+                id="navbar-collapse2",
+                navbar=True,
+            ),
+        ]
+    ),
+    color="dark",
+    dark=True,
+    className="mb-5",
+)
+"""NavBar End"""
+
+###########################################
+
+"""App Components Start"""
+# body part
+
+# Cards for TTL, DNC, ATC
+cardTTL = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H4("COVID-19 Cases", className="card-title"),
+            html.H2(TTL, className="card-subtitle"),
+        ]
+    ),
+    style={"width": "18rem"},
+)
+cardDNC = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H4("COVID-19 Cases", className="card-title"),
+            html.H2(DNC, className="card-subtitle"),
+        ]
+    ),
+    style={"width": "18rem"},
+)
+
+cardATC = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H4("COVID-19 Cases", className="card-title"),
+            html.H2(ATC, className="card-subtitle"),
+        ]
+    ),
+    style={"width": "18rem"},
+)
+
+topTenGraph = html.Div(
+    [
+        dcc.Graph(
+            id="topTenGraph",
+            figure=go.Figure(go.Bar(x=xValue, y=yValue, orientation="h")),
         )
-        return fig
-    elif graphSelection == "DNC":
-        fig = make_subplots(rows=1, cols=1)
-        fig.add_trace(
-            go.Bar(x=dailyGlobalNewCases.index, y=dailyGlobalNewCases[countryDropdown]),
-            row=1,
-            col=1,
+    ],
+    className="topTenGraph",
+)
+
+graphConfirmed = html.Div(
+    [
+        dcc.Graph(
+            id="confirmedGraph",
+            figure=go.Figure(
+                data=[
+                    go.Bar(
+                        x=dfConfirmed.index,
+                        y=dfConfirmed[yValue[-1]],
+                    )
+                ],
+                layout=go.Layout(
+                    title=go.layout.Title(text=yValue[-1] + " Total Confirmed Cases")
+                ),
+            ),
         )
-        return fig
-    elif graphSelection == "ATC":
-        fig = make_subplots(rows=1, cols=1)
-        fig.add_trace(
-            go.Bar(x=dfActive.index, y=dfActive[countryDropdown]),
-            row=1,
-            col=1,
+    ],
+    className="graph",
+)
+graphDailyNew = html.Div(
+    [
+        dcc.Graph(
+            id="dailyNewGraph",
+            figure=go.Figure(
+                data=[
+                    go.Bar(
+                        x=dailyGlobalNewCases.index,
+                        y=dailyGlobalNewCases[yValue[-1]],
+                    )
+                ],
+                layout=go.Layout(
+                    title=go.layout.Title(text=yValue[-1] + " Daily New Cases")
+                ),
+            ),
         )
-        return fig
+    ],
+    className="graph",
+)
+
+graphActive = html.Div(
+    [
+        dcc.Graph(
+            id="activeGraph",
+            figure=go.Figure(
+                data=[
+                    go.Bar(
+                        x=dfActive.index,
+                        y=dfActive[yValue[-1]],
+                    )
+                ],
+                layout=go.Layout(
+                    title=go.layout.Title(text=yValue[-1] + " Active Cases")
+                ),
+            ),
+        )
+    ],
+    className="graph",
+)
 
 
+###############################
+dropdown = html.Div(
+    [
+        dcc.Dropdown(
+            id="countryDropdown",
+            options=optCountries,
+        )
+    ],
+    className="dropdown",
+)
+
+radioItems = html.Div(
+    [
+        dcc.RadioItems(
+            options=[
+                {
+                    "label": "Total Cases",
+                    "value": "TTC",
+                },
+                {
+                    "label": "Daily New Cases",
+                    "value": "DNC",
+                },
+                {
+                    "label": "Active Cases",
+                    "value": "ATC",
+                },
+            ],
+            id="graphSelection",
+            value="TTC",
+            labelStyle={"display": "inline-block"},
+        )
+    ],
+    className="radioItems",
+)
+
+graph = html.Div(
+    [
+        dcc.Graph(
+            id="chosenGraph",
+            figure=go.Figure(
+                data=[
+                    go.Bar(
+                        x=dfConfirmed.index,
+                        y=dfConfirmed[yValue[-1]],
+                    )
+                ],
+                layout=go.Layout(title=go.layout.Title(text=yValue[-1])),
+            ),
+        )
+    ],
+    className="graph",
+)
+
+
+"""App Components End"""
+
+###########################################
+
+"""App Layout"""
+app.layout = html.Div(
+    [
+        navbar,
+        dbc.Container(
+            dbc.Row(
+                [
+                    dbc.Col([cardTTL, cardDNC, cardATC], width=3),
+                    dbc.Col(topTenGraph, width=3),
+                    dbc.Col(
+                        [
+                            graphConfirmed,
+                            graphDailyNew,
+                            graphActive,
+                        ],
+                        width=6,
+                    ),
+                ]
+            )
+        ),
+    ]
+)
+
+"""Callback Start"""
+
+
+"""Callback End"""
 
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True)
